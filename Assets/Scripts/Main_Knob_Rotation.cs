a66ff283fbf62d1daafc57573617277094eed6db
Alexis V <133908521+Marimboi3@users.noreply.github.com>
2023-12-19 17:01:02 -0800

    Rotation Mechanism works now
    I just need to add the message offset change

--- Assets/Scripts/Main_Knob_Rotation.cs
+++ Assets/Scripts/Main_Knob_Rotation.cs
@@ -10,7 +10,11 @@ public class Main_Knob_Rotation : MonoBehaviour
 
     private bool isRotating = false;
     private char knobSelectedLetter;
+    private int knobASCII;
     private char baseSelectedLetter;
+    private int baseASCII;
+    private int knobOffset = 0;
+    private float currentRotation = 0;
 
     private void Update()
     {
@@ -26,73 +30,84 @@ public class Main_Knob_Rotation : MonoBehaviour
                 if (clickedObject.CompareTag("MainKnob"))
                 {
                     knobSelectedLetter = clickedObject.GetComponent<LetterInfo>().letter;
+                    Debug.Log("Actual Letter Chosen: " + knobSelectedLetter);
+                    knobASCII = (int)knobSelectedLetter + knobOffset;
+
+                    if (knobASCII > 90)
+                    {
+                        knobASCII -= 26;
+                    }
+                    else if (knobASCII < 65)
+                    {
+                        knobASCII += 26;
+                    }
+
+                    Debug.Log("Offset Knob Letter: " + (char)knobASCII);
+                    Debug.Log("Offset ASCII: " + knobASCII);
                 }
                 else if (clickedObject.CompareTag("MainBase"))
                 {
                     baseSelectedLetter = clickedObject.GetComponent<LetterInfo>().letter;
+                    Debug.Log("Selected Base Letter: " + baseSelectedLetter);
+                    baseASCII = (int)baseSelectedLetter;
+                    Debug.Log("Letter ASCII: " + baseASCII);
                 }
             }
 
             if (!isRotating && knobSelectedLetter != '\0' && baseSelectedLetter != '\0')
             {
+                int localChange = knobASCII - baseASCII;
+                Debug.Log("Message Offset: " +  localChange);
+
                 isRotating = true;
                 RotateToSelectedLetters();
+
+                knobSelectedLetter = '\0';
+                baseSelectedLetter = '\0';
             }
         }
     }
 
     private void RotateToSelectedLetters()
     {
-        int knobIndex = knobSelectedLetter - 'A';
-        int baseIndex = baseSelectedLetter - 'A';
+        int knobIndex = knobASCII - 'A';
+        int baseIndex = baseASCII - 'A';
+
+        Debug.Log("knob index: " + knobIndex);
+        Debug.Log("base index: " + baseIndex);
 
         float anglePerLetter = 360f / 26;
-        float targetRotation = knob.rotation.eulerAngles.z + (baseIndex - knobIndex) * anglePerLetter;
 
-        Quaternion targetQuaternion = Quaternion.Euler(0f, 0f, targetRotation);
+        Debug.Log("Rotation Before: " + currentRotation);
+
+        float targetRotation = currentRotation + (baseIndex - knobIndex) * anglePerLetter;
+
+        knobOffset = Mathf.RoundToInt(targetRotation / anglePerLetter);
+
+        //Debug.Log("Target Rotation: " + targetRotation);
+        //Debug.Log("Positiones Moved: " + targetRotation / anglePerLetter);
+        //Debug.Log("Knob Offset: " + knobOffset);
+
+        Quaternion targetQuaternion = Quaternion.Euler(10f, 0f, targetRotation);
 
         StartCoroutine(RotateKnob(targetQuaternion));
     }
 
     private IEnumerator RotateKnob(Quaternion targetRotation)
     {
-        while (Quaternion.Angle(knob.rotation, targetRotation) > 0.1f)
+        while (Quaternion.Angle(knob.localRotation, targetRotation) > 0.1f)
         {
-            knob.rotation = Quaternion.Slerp(knob.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            //Debug.Log(Quaternion.Angle(knob.localRotation, targetRotation));
+            //Debug.Log("Current Rotation: " + knob.localRotation.eulerAngles);
+            knob.localRotation = Quaternion.Slerp(knob.localRotation, targetRotation, rotationSpeed * Time.deltaTime);
             yield return null;
         }
 
         isRotating = false;
+
+        currentRotation = knob.localRotation.eulerAngles.z;
+        Debug.Log("Rotation After: " + currentRotation);
     }
-
-/*    private Vector3 GetMouseWorldPosition()
-    {
-        Debug.Log(isRotating);
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit))
-        {
-            return hit.point;
-        }
-
-        return Vector3.zero;
-    }
-
-    private void RotateTowardsEndLocation()
-    {
-        Vector3 localEndLocation = transform.InverseTransformPoint(endLocation);
-        float targetAngle = Mathf.Atan2(localEndLocation.y, localEndLocation.x) * Mathf.Rad2Deg;
-
-        Quaternion targetRotation = Quaternion.Euler(0f, 0f, targetAngle);
-        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, rotationSpeed * Time.deltaTime);
-
-        if (Quaternion.Angle(transform.localRotation, targetRotation) < 0.1f)
-        {
-            isRotating = false;
-            Debug.Log("Stop Rotating!");
-        }
-    }*/
 
    /* public int letterChange(int last, int current)
     {
